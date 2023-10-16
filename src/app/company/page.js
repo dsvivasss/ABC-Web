e16440542a9067ff2342968e86983a0e52c5c058
Mateo-Olaya <m.olaya11@uniandes.edu.co>
2023-10-15 22:33:59 -0500

    PR-7 Se corrige cambio en unica pagina

--- src/app/company/page.js
+++ src/app/company/page.js
@@ -87,7 +87,6 @@ export default function Company() {
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
             href="/"
-            target="_blank"
             rel="noopener noreferrer"
           >
             <p>Accede como aspirante</p>
@@ -95,7 +94,6 @@ export default function Company() {
           <a
             className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
             href="/company"
-            target="_blank"
             rel="noopener noreferrer"
           >
             <p>
--- src/app/page.js
+++ src/app/page.js
@@ -49,7 +49,6 @@ export default function Home() {
           <a
             className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
             href="/"
-            target="_blank"
             rel="noopener noreferrer"
           >
             <p>Accede como aspirante</p>
@@ -57,7 +56,6 @@ export default function Home() {
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
             href="/company"
-            target="_blank"
             rel="noopener noreferrer"
           >
             <p>
