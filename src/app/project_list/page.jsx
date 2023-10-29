7f26d6b82ec78a154845cafaea7b2d8dc6a11ad3
Mateo-Olaya <m.olaya11@uniandes.edu.co>
2023-10-28 22:47:45 -0500

    PR-9 Se ajusta ventana que lista los proyectos para que consuma el enpoint

--- src/app/project_list/page.jsx
+++ src/app/project_list/page.jsx
@@ -7,6 +7,7 @@ import { Disclosure, Menu } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { PaperClipIcon } from "@heroicons/react/20/solid";
 import { useCallback, useEffect } from "react";
+import axios from "axios";
 import {
   BriefcaseIcon,
   CalendarIcon,
@@ -17,6 +18,8 @@ import {
   MapPinIcon,
   PencilIcon,
 } from "@heroicons/react/20/solid";
+
+import ProjectcardItem from "../components/ProjectcardItem";
 
 const proyectosEjemplo = [
   {
@@ -62,6 +65,27 @@ function classNames(...classes) {
 }
 
 export default function Project() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => ListProject(), []);
+
+  function ListProject() {
+    fetch(
+      "https://fli2mqd2g8.execute-api.us-east-1.amazonaws.com/dev/projects/companies/2",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+
+      .then((data) => setProjects(data.projects));
+  }
+
+  // {projects=data.projects}
+
   return (
     <>
       <div class="">
@@ -298,355 +322,17 @@ export default function Project() {
             </div>
           </div>
 
-          <div className={styles.card}>
-            <div className="lg:flex lg:items-center lg:justify-between pb-2">
-              <div className="min-w-0 flex-1">
-                <h2 className="animate-fade-up from-black bg-clip-text text-1xl leading-7 text-gray-900 sm:truncate sm:text-2xl sm:tracking-tight">
-                  Migración del aplicativo Murex3
-                </h2>
-                <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
-                  <div className="mt-2 flex items-center text-sm text-gray-500">
-                    <BriefcaseIcon
-                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    Full-time
-                  </div>
-                  <div className="mt-2 flex items-center text-sm text-gray-500">
-                    <MapPinIcon
-                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    Remote
-                  </div>
-                  <div className="mt-2 flex items-center text-sm text-gray-500">
-                    <CurrencyDollarIcon
-                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    $120k &ndash; $140k
-                  </div>
-                  <div className="mt-2 flex items-center text-sm text-gray-500">
-                    <CalendarIcon
-                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    Closing on January 9, 2020
-                  </div>
-                </div>
-              </div>
-              <div className="mt-5 flex lg:ml-4 lg:mt-0">
-                <span className="hidden sm:block">
-                  <button
-                    type="button"
-                    className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                  >
-                    <PencilIcon
-                      className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    Editar
-                  </button>
-                </span>
-
-                <span className="ml-3 hidden sm:block">
-                  <button
-                    type="button"
-                    className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                  >
-                    <LinkIcon
-                      className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    Ver
-                  </button>
-                </span>
-
-                {/* Dropdown */}
-                <Menu as="div" className="relative ml-3 sm:hidden">
-                  <Menu.Button className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400">
-                    More
-                    <ChevronDownIcon
-                      className="-mr-1 ml-1.5 h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </Menu.Button>
-
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 -mr-1 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Editar
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Ver
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.card}>
-            <div className="lg:flex lg:items-center lg:justify-between pb-2">
-              <div className="min-w-0 flex-1">
-                <h2 className="animate-fade-up from-black bg-clip-text text-1xl leading-7 text-gray-900 sm:truncate sm:text-2xl sm:tracking-tight">
-                  Modernización del sistema de información Dialogo
-                </h2>
-                <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
-                  <div className="mt-2 flex items-center text-sm text-gray-500">
-                    <BriefcaseIcon
-                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    Full-time
-                  </div>
-                  <div className="mt-2 flex items-center text-sm text-gray-500">
-                    <MapPinIcon
-                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    Remote
-                  </div>
-                  <div className="mt-2 flex items-center text-sm text-gray-500">
-                    <CurrencyDollarIcon
-                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    $120k &ndash; $140k
-                  </div>
-                  <div className="mt-2 flex items-center text-sm text-gray-500">
-                    <CalendarIcon
-                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    Closing on January 9, 2020
-                  </div>
-                </div>
-              </div>
-              <div className="mt-5 flex lg:ml-4 lg:mt-0">
-                <span className="hidden sm:block">
-                  <button
-                    type="button"
-                    className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                  >
-                    <PencilIcon
-                      className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    Editar
-                  </button>
-                </span>
-
-                <span className="ml-3 hidden sm:block">
-                  <button
-                    type="button"
-                    className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                  >
-                    <LinkIcon
-                      className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    Ver
-                  </button>
-                </span>
-
-                {/* Dropdown */}
-                <Menu as="div" className="relative ml-3 sm:hidden">
-                  <Menu.Button className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400">
-                    More
-                    <ChevronDownIcon
-                      className="-mr-1 ml-1.5 h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </Menu.Button>
-
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 -mr-1 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Editar
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Ver
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.card}>
-            <div className="lg:flex lg:items-center lg:justify-between pb-2">
-              <div className="min-w-0 flex-1">
-                <h2 className="animate-fade-up from-black bg-clip-text text-1xl leading-7 text-gray-900 sm:truncate sm:text-2xl sm:tracking-tight">
-                  Nueva aplicación móvil para Disney Inc.
-                </h2>
-                <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
-                  <div className="mt-2 flex items-center text-sm text-gray-500">
-                    <BriefcaseIcon
-                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    Full-time
-                  </div>
-                  <div className="mt-2 flex items-center text-sm text-gray-500">
-                    <MapPinIcon
-                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    Remote
-                  </div>
-                  <div className="mt-2 flex items-center text-sm text-gray-500">
-                    <CurrencyDollarIcon
-                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    $120k &ndash; $140k
-                  </div>
-                  <div className="mt-2 flex items-center text-sm text-gray-500">
-                    <CalendarIcon
-                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    Closing on January 9, 2020
-                  </div>
-                </div>
-              </div>
-              <div className="mt-5 flex lg:ml-4 lg:mt-0">
-                <span className="hidden sm:block">
-                  <button
-                    type="button"
-                    className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                  >
-                    <PencilIcon
-                      className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    Editar
-                  </button>
-                </span>
-
-                <span className="ml-3 hidden sm:block">
-                  <button
-                    type="button"
-                    className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                  >
-                    <LinkIcon
-                      className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    Ver
-                  </button>
-                </span>
-
-                {/* Dropdown */}
-                <Menu as="div" className="relative ml-3 sm:hidden">
-                  <Menu.Button className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400">
-                    More
-                    <ChevronDownIcon
-                      className="-mr-1 ml-1.5 h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </Menu.Button>
-
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 -mr-1 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Editar
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Ver
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
-            </div>
+          <div>
+            {projects.map((project, index) => (
+              <ProjectcardItem
+                company_id={index}
+                title={project.title}
+                description={project.description}
+                soft_skills={project.soft_skills}
+                hard_skills={project.hard_skills}
+                roles={project.roles}
+              />
+            ))}
           </div>
         </div>
       </div>
