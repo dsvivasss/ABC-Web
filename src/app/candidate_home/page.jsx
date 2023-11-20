66a41854a28cf03887fca9d38e030fe26f60e921
dsvivass <dsvivass@unal.edu.co>
2023-11-19 19:53:18 -0500

    Versioning untracked files

--- src/app/candidate_home/page.jsx
+++ src/app/candidate_home/page.jsx
@@ -5,201 +5,51 @@ import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { Disclosure, Menu } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { PaperClipIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { PaperClipIcon } from "@heroicons/react/20/solid";
 import { useCallback, useEffect } from "react";
-import { CheckIcon } from "@heroicons/react/20/solid";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import TestcardItem from "../components/TestcardItem";
-import Link from "next/link";
-import Datepicker from "react-tailwindcss-datepicker";
-import TimePicker from "../components/TimePicker";
- 
+import {
+  BriefcaseIcon,
+  CalendarIcon,
+  CheckIcon,
+  ChevronDownIcon,
+  CurrencyDollarIcon,
+  LinkIcon,
+  MapPinIcon,
+  PencilIcon,
+} from "@heroicons/react/20/solid";
 
-const skills_soft = [
-  {
-    id: 1,
-    name: "Comunicación estratégica",
-  },
- 
+import ProjectcardItem from "../components/ProjectcardItem";
+import Link from 'next/link'
 
-  {
-    id: 2,
-    name: "Pensamiento sistémico",
-  },
- 
-
-  {
-    id: 3,
-    name: "Creatividad",
-  },
-];
- 
-
-const type = [
-  {
-    id: "technical",
-    name: "Tecnica",
-  },
- 
-
-  {
-    id: "psychology",
-    name: "Pisicolgia",
-  },
-];
- 
-
-const dificultad = [
-  {
-    id: "hard",
-    name: "Alta",
-  },
- 
-
-  {
-    id: "medium",
-    name: "Media",
-  },
- 
-
-  {
-    id: "basic",
-    name: "Baja",
-  },
-];
- 
-
-const topics = [
-  {
-    id: "python",
-    name: "Python",
-  },
-  {
-    id: "javascript",
-    name: "Javascript",
-  },
-  {
-    id: "java",
-    name: "Java",
-  },
-  {
-    id: "oop",
-    name: "Programación orientada a objetos",
-  },
-  {
-    id: "product_management",
-    name: "Product management",
-  },
-];
-
-const navigation = [{ name: "Dashboard", href: "#", current: true }];
 
 // istanbul ignore next
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
- 
 
-export default function Interview_create() {
-  const [selected, setSelected] = useState(skills_soft[0]);
-  const [selected2, setSelected2] = useState(type[0]);
-  const [selected3, setSelected3] = useState(dificultad[0]);
-  const [selected4, setSelected4] = useState(topics[0]);
-  const [tests, setTests] = useState([]);
-  var company_id = null;
-  var project_data = null;
- 
+export default function Project() {
+  const [projects, setProjects] = useState([]);
+  var company_id = null
+  useEffect(() => {
+    company_id = localStorage.getItem("company_id")
+    // ListProject()
+  }, []);
 
-  const [value, setValue] = useState({
-    startDate: new Date(),
-    endDate: new Date().setMonth(11),
-  });
- 
-
-  const handleValueChange = (newValue) => {
-    console.log("newValue:", newValue);
-    setValue(newValue);
-  };
- 
-
-  // istanbul ignore next
-  function register(e) {
-    const company = 1;
-    e.preventDefault();
-    const form = e.target;
-    const formData = new FormData(form);
-    const formJson = Object.fromEntries(formData.entries());
-    const body = {
-      company_id: +company_id,
-      project_id: project_data.id,
-      title: formJson.test_title,
-      type: selected2.id,
-      difficulty_level: selected3.id,
-      hard_skills: [selected4.id],
-      questions: tests.map((test) => test.id),
-    };
- 
-
-    // istanbul ignore next
-    fetch("https://fli2mqd2g8.execute-api.us-east-1.amazonaws.com/dev/tests/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        toast("Prueba creada!", { position: "bottom-left", theme: "dark" });
-      });
-  }
- 
-
-  // istanbul ignore next
-  async function getTests() {
-    const body = {
-      topics: [selected4.id], // {#options: ["python", "javascript", "java", "oop", "product_management"]#}
-      difficulty_level: selected3.id, // {#options: ["basic", "medium", "hard"]#}
-      question_type: "multiple_choice",
-      options: {
-        // {#Optional field#}
-        return_answers: true, // {#Default is false#}
-      },
-    };
- 
-
-    const request = await fetch(
-      "https://fli2mqd2g8.execute-api.us-east-1.amazonaws.com/dev/questions/",
+  function ListProject() {
+    
+    fetch(
+      `https://fli2mqd2g8.execute-api.us-east-1.amazonaws.com/dev/projects/companies/${company_id}`,
       {
-        method: "POST",
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(body),
       }
-    );
-
-    const response = await request.json();
-
-    setTests(response);
+    )
+      .then((response) => response.json())
+      .then((data) => {console.log(data)
+        setProjects(data.projects)});
   }
-
-  useEffect(() => {
-    company_id = localStorage.getItem("company_id");
-    project_data = JSON.parse(localStorage.getItem("project_selected"));
-  });
- 
-
-  useEffect(() => {
-    async function getTestsCall() {
-      await getTests();
-    }
-
-    getTestsCall();
-  }, [selected3, selected4]);
-
   return (
     <>
       <div className="">
@@ -238,7 +88,6 @@ export default function Interview_create() {
                       <span className="sr-only">Ver notifications</span>
                       <BellIcon className="h-6 w-6" aria-hidden="true" />
                     </button>
- 
 
                     {/* Profile dropdown */}
                     <Menu as="div" className="relative ml-3">
@@ -324,11 +173,10 @@ export default function Interview_create() {
                       </div>
                     </div>
                   </li>
- 
 
                   <li>
                     <a
-                      href="/project_list"
+                      href="#"
                       className="flex items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6"
                     >
                       <span className="inline-flex justify-center items-center ml-4">
@@ -348,10 +196,10 @@ export default function Interview_create() {
                         </svg>
                       </span>
                       <span className="ml-2 text-sm tracking-wide truncate">
-                        Proyectos
+                        pruebas
                       </span>
                       <span className="px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-green-500 bg-green-50 rounded-full">
-                        3
+                        {projects.length}
                       </span>
                     </a>
                   </li>
@@ -360,78 +208,79 @@ export default function Interview_create() {
             </div>
           </div>
         </div>
- 
 
         <div className="w-5/6 p-4">
           <div className="lg:flex lg:items-center lg:justify-between">
             <div className="min-w-0 flex-1 pl-6 pb-4">
               <h2 className="animate-fade-up from-black bg-clip-text text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight py-2">
-                Programa una nueva entrevista a su candidato seleccionado
+                Mis pruebas
               </h2>
-              <p>Estas a muy pocos pasos de conocer a tu candidato</p>
+              <p>
+                A continuación podrás ver el listado de tus pruebas en curso.
+              </p>
+            </div>
+            <div className="mt-5 flex lg:ml-4 lg:mt-0">
+
+              {/* Dropdown */}
+              <Menu as="div" className="relative ml-3 sm:hidden">
+                <Menu.Button className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400">
+                  More
+                  <ChevronDownIcon
+                    className="-mr-1 ml-1.5 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </Menu.Button>
+
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-200"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 z-10 -mr-1 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          href="#"
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          Editar
+                        </a>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          Ver
+                        </a>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
             </div>
           </div>
- 
 
-          <div className={styles.grid2}>
-            <div className={styles.card}>
-              <div className="lg:flex lg:items-center lg:justify-between pb-2">
-                <div className="min-w-0 flex-1">
-                  <form className="space-y-6" onSubmit={register}>
-                    <div>
-                      <label
-                        htmlFor="test_title"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        Fecha
-                      </label>
-                      <div className="mt-2">
-                      <div>
-                        <Datepicker
-                          value={value}
-                          onChange={handleValueChange}
-                          asSingle={true}
-                          useRange={false}
-                          primaryColor={"indigo"}
-                        />
-                      </div>  
-                      </div>
-                      <label
-                        htmlFor="test_title"
-                        className="block text-sm font-medium leading-6 text-gray-900 pt-2"
-                      >
-                        Hora
-                      </label>
-                      <div>
-                      <TimePicker></TimePicker>
-                      </div>
-
-                    </div>
-
-                    <div className="pb-1 pt-2">
-                      <span className="hidden sm:block pr-5">
-                        <button
-                          type="button"
-                          className="inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold text-indigo-800 leading-6"
-                        >
-                          <Link href="/project_detail">Cancelar</Link>
-                        </button>
-                      </span>
-                      <button
-                        type="submit"
-                        className="justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                      >
-                        Citar entrevista
-                      </button>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
+          <div>
+            {projects.map((project, index) => (
+              <ProjectcardItem 
+              project_data={project}
+              />
+            ))}
           </div>
         </div>
       </div>
-      <ToastContainer />
     </>
   );
 }
